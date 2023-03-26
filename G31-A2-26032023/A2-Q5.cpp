0ac2e83739d0e29ae1ae4a73b4d8abee4fd46587
Cora <78966342+coradora@users.noreply.github.com>
2023-03-26 12:25:26 -0400

    import other assignments

---
+++ G31-A2-26032023/A2-Q5.cpp
@@ -0,0 +1,87 @@
+/// Utilize c++'s standard thread library to perform some statistical analysis on a list of numbers fed via command line arguments
+#include <thread>
+#include <list>
+#include <iostream>
+using namespace std;
+
+// Globals 
+int globalMin;
+int globalMax;
+double globalMean;
+
+// function declarations
+void findMax(list<int> numbers);
+void findMin(list<int> numbers);
+void findMean(list<int> numbers);
+
+int main(int argc, char* argv[]) {
+    if (argc < 1)
+    {
+        cerr << "Must pass at least one integer command line argument" << endl;
+        return 1;
+    }
+    list<int> numbers;
+
+    //add each command line argument to our list
+    for (int i = 1; i < argc; i++)
+    {
+        numbers.push_back(atoi(argv[i]));
+    }
+    //define threads for our tasks
+    thread maxThread(findMax, numbers);
+    thread minThread(findMin, numbers);
+    thread meanThread(findMean, numbers);
+
+    //startup threads
+    maxThread.join();
+    minThread.join();
+    meanThread.join();
+
+    //the threads will have prepared our global variables with the values they need
+    cout << "Minimum number is " << globalMin << endl;
+    cout << "Maximum number is " << globalMax << endl;
+    cout << "Mean number is " << globalMean << endl;
+    return 0;
+}
+/// <summary>
+/// find the average of a list of numbers
+/// </summary>
+/// <param name="numbers"></param>
+void findMean(list<int> numbers) {
+    double sum = 0;
+    for (const auto& number : numbers)
+    {
+        sum += number;
+    }
+    globalMean = sum / numbers.size();
+}
+/// <summary>
+/// find the smallest number in a list
+/// </summary>
+/// <param name="numbers"></param>
+void findMin(list<int> numbers) {
+    int min = numbers.front();
+    for (const auto& number : numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+    }
+    globalMin =  min;
+}
+/// <summary>
+/// find the largest number in a list
+/// </summary>
+/// <param name="numbers"></param>
+void findMax(list<int> numbers) {
+    int max = numbers.front();
+    for (const auto& number : numbers)
+    {
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+    globalMax = max;
+}
